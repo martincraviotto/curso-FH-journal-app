c8ca34d3004948bf3628742dc5ca1921f06953a6
Martin <martincraviotto@gmail.com>
2023-01-25 19:10:28 -0300

    Final Clase244 - Auth Layout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,24 +2,12 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <Grid container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{minHeight:'100vh',backgroundColor:'primary.main', padding:4}}
-    >
-      <Grid item
-        className='box-shadow'
-        xs={3}
-        sx={{ backgroundColor:'white', padding:3, borderRadius:2}}
-      >
-        <Typography variante='h5' sx={{mb:1}}> Login </Typography>
-
-        <form action="">
+    <AuthLayout title='Login'>
+      <form action="">
           <Grid container>
             <Grid item xs={12} sx={{mt:2}}>
               <TextField 
@@ -50,10 +38,7 @@ export const LoginPage = () => {
 
             </Grid>
 
-            <Grid container
-              direction='row'
-              justifyContent='end'
-            >
+            <Grid container direction='row' justifyContent='end'>
               <Link component={RouterLink} color='inherit' to="/auth/register">
                 Crear una cuenta
               </Link>
@@ -61,7 +46,10 @@ export const LoginPage = () => {
 
           </Grid>
         </form>
-      </Grid>
-    </Grid>
+
+    </AuthLayout>
+
+        
+      
   )
 }
