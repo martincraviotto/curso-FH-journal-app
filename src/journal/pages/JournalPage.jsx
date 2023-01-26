5f12dc73f41b9c439bc38281e77a2aa9141180ae
Martin Craviotto <martincraviotto@gmail.com>
2023-01-26 11:08:04 -0300

    Final Clase252 - Final Seccion 17- Journalapp - MaterialUI - Estructura y Diseño

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,33 @@
 
-import { Typography } from '@mui/material'
 import React from 'react'
+import { IconButton, Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NoteView, NothingSelectedView } from '../views'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       
-      <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quo officiis minus provident aperiam error in esse sit itaque rerum corporis, temporibus neque dolorem reprehenderit ipsum modi minima dolorum nesciunt.</Typography>
+      {/* <Typography>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quo officiis minus provident aperiam error in esse sit itaque rerum corporis, temporibus neque dolorem reprehenderit ipsum modi minima dolorum nesciunt.</Typography> */}
 
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton 
+        size='large'
+        sx={{
+            color: 'white',
+             backgroundColor:'error.main',
+             ':hover' : {backgroundColor:'error.main', opacity:0.9},
+             position: 'fixed',
+             right: 50,
+             bottom:50
+            }}
+      >
+          <AddOutlined sx={{fontSize:30}}/>
+
+      </IconButton>
 
 
     </JournalLayout>
