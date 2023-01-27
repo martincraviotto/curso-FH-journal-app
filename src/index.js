d61a5461ca509dcc251bf09917884d57bc3cdf13
Martin Craviotto <martincraviotto@gmail.com>
2023-01-27 12:03:03 -0300

    Final Clase274

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { JuornalApp } from './JuornalApp';
+import { store } from './store';
 
 import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JuornalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JuornalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
